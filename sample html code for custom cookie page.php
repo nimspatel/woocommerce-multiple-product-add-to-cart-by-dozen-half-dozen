74ae761b96ab096dc659d4d7afa86fae31b10dad
Nimesh Patel Senior Web Developer <patel.nimesh003@gmail.com>
2022-07-31 16:12:18 +0530

    Add files via upload

---
+++ sample html code for custom cookie page.php
@@ -0,0 +1,109 @@
+<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-6ea7501" data-id="6ea7501" data-element_type="column" data-settings="{&quot;mdp_selection_sticky_column_effect_enable&quot;:false}">
+            <div class="elementor-widget-wrap elementor-element-populated">
+                                <section class="elementor-section elementor-inner-section elementor-element elementor-element-0bf0ed8 elementor-section-boxed elementor-section-height-default" data-id="0bf0ed8" data-element_type="section" data-settings="{&quot;mdp_selection_sticky_effect_enable&quot;:false}">
+                        <div class="elementor-container elementor-column-gap-default">
+                    <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-ee6c188" data-id="ee6c188" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;,&quot;mdp_selection_sticky_column_effect_enable&quot;:false}">
+            <div class="elementor-widget-wrap elementor-element-populated">
+                                <div class="elementor-element elementor-element-25f9bb6 elementor-widget elementor-widget-heading" data-id="25f9bb6" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h3 class="elementor-heading-title elementor-size-default">One Dozen</h3>       </div>
+                </div>
+                <div class="elementor-element elementor-element-f3666e8 elementor-widget elementor-widget-heading" data-id="f3666e8" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h4 class="elementor-heading-title elementor-size-default">12 Cookies</h4>      </div>
+                </div>
+                <div class="elementor-element elementor-element-88f98b8 elementor-widget elementor-widget-heading" data-id="88f98b8" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h4 class="elementor-heading-title elementor-size-default">$.98/oz • $62.95</h4>        </div>
+                </div>
+                <div class="elementor-element elementor-element-6b709ba elementor-align-center size-dozen-button-scroll elementor-widget elementor-widget-button" data-id="6b709ba" data-element_type="widget" data-widget_type="button.default">
+                <div class="elementor-widget-container">
+                    <div class="elementor-button-wrapper">
+            <a href="#" class="elementor-button-link elementor-button elementor-size-sm" role="button" id="tcookies">
+                        <span class="elementor-button-content-wrapper">
+                        <span class="elementor-button-text">SELECT FLAVORS</span>
+        </span>
+                    </a>
+        </div>
+                </div>
+                </div>
+                    </div>
+        </div>
+                <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-7d3acd2" data-id="7d3acd2" data-element_type="column" data-settings="{&quot;background_background&quot;:&quot;classic&quot;,&quot;mdp_selection_sticky_column_effect_enable&quot;:false}">
+            <div class="elementor-widget-wrap elementor-element-populated">
+                                <div class="elementor-element elementor-element-6af65ae elementor-widget elementor-widget-heading" data-id="6af65ae" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h3 class="elementor-heading-title elementor-size-default">Half Dozen</h3>      </div>
+                </div>
+                <div class="elementor-element elementor-element-5f6ede9 elementor-widget elementor-widget-heading" data-id="5f6ede9" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h4 class="elementor-heading-title elementor-size-default">6 Cookies</h4>       </div>
+                </div>
+                <div class="elementor-element elementor-element-6cca077 elementor-widget elementor-widget-heading" data-id="6cca077" data-element_type="widget" data-widget_type="heading.default">
+                <div class="elementor-widget-container">
+            <h4 class="elementor-heading-title elementor-size-default">$1.22/oz • $43.95</h4>       </div>
+                </div>
+                <div class="elementor-element elementor-element-452dd5d elementor-align-center size-half-button-scroll elementor-widget elementor-widget-button" data-id="452dd5d" data-element_type="widget" data-widget_type="button.default">
+                <div class="elementor-widget-container">
+                    <div class="elementor-button-wrapper">
+            <a href="#" class="elementor-button-link elementor-button elementor-size-sm" role="button" id="scookies">
+                        <span class="elementor-button-content-wrapper">
+                        <span class="elementor-button-text">SELECT FLAVORS</span>
+        </span>
+                    </a>
+        </div>
+                </div>
+                </div>
+                    </div>
+        </div>
+                            </div>
+        </section>
+                    </div>
+        </div>
+
+
+
+      
+      <?php do_shortcode('[product_list_without_price slug="vegan"]'); ?>  
+
+
+
+    <div class="picked_cookies cookies-picked-container">You picked <span class="current_amount_of_cookies">0</span> cookie(s) out of <span class="total_of_cookies_limit">0</span></div>
+
+    <form class="variations_form cart swatches-support" action="https://louiss36.sg-host.com/product/pick-your-mix-wegen/" method="post" enctype="multipart/form-data" data-product_id="10265" current-image="8925">
+    
+            <table class="variations" cellspacing="0">
+            <tbody>
+                                                                            <tr>
+                        <td class="label"><label for="quantity">Quantity</label></td>
+                        <td class="value ">
+                            <select id="quantity" class=" hide rtwpvs-wc-select rtwpvs-wc-type-button" style="display:none" name="attribute_quantity" data-attribute_name="attribute_quantity" data-show_option_none="yes"><option value="">Choose an option</option><option value="One Dozen" class="attached enabled">One Dozen</option><option value="Half Dozen" class="attached enabled">Half Dozen</option></select><div class="rtwpvs-terms-wrapper button-variable-wrapper" data-attribute_name="attribute_quantity"><div data-rtwpvs-tooltip="One Dozen" class="rtwpvs-term rtwpvs-button-term button-variable-term-One Dozen selected" title="One Dozen" data-term="One Dozen"><span class="rtwpvs-term-span rtwpvs-term-span-button">One Dozen</span></div><div data-rtwpvs-tooltip="Half Dozen" class="rtwpvs-term rtwpvs-button-term button-variable-term-Half Dozen" title="Half Dozen" data-term="Half Dozen"><span class="rtwpvs-term-span rtwpvs-term-span-button">Half Dozen</span></div></div><a class="reset_variations" href="#" style="visibility: visible;">Clear</a>                        </td>
+                    </tr>
+                            </tbody>
+        </table>
+
+        <div class="single_variation_wrap">
+            <div class="woocommerce-variation single_variation" style="display: block;">
+    <div class="woocommerce-variation-description"><p>12 Cookies</p>
+</div>
+    <div class="woocommerce-variation-price"><span class="price"><span class="woocommerce-Price-amount amount"><bdi><span class="woocommerce-Price-currencySymbol">$</span>62.00</bdi></span></span></div>
+    <div class="woocommerce-variation-availability"><p class="stock in-stock">670 in stock</p>
+</div>
+</div><div class="woocommerce-variation-add-to-cart variations_button woocommerce-variation-add-to-cart-enabled">
+    <input type="hidden" name="picked_cookies" id="picked_cookies" value="">
+        <div class="quantity woopq-quantity woopq-quantity-default woopq-type-default" data-min="1" data-max="670" data-step="1" data-value="1" style="display: block;">
+        <input class="minus" type="button" value="-">        <label class="screen-reader-text" for="quantity_62e65abc833a9">
+            Pick your mix wegen quantity        </label>
+                    <input type="number" id="quantity_62e65abc833a9" class="input-text qty text" step="1" min="1" max="670" name="quantity" value="1" title="Qty" size="4" placeholder="" inputmode="numeric">
+                    <input class="plus" type="button" value="+">    </div>
+    
+    <button type="submit" class="single_add_to_cart_button button alt disabled">Add to cart</button>
+
+    
+    <input type="hidden" name="add-to-cart" value="10265">
+    <input type="hidden" name="product_id" value="10265">
+    <input type="hidden" name="variation_id" class="variation_id" value="10266">
+</div>
+        </div>
+    
+    </form>
